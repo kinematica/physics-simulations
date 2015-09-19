4ec87edd06e138895bb591cfa2b7db7b648b16ce
Stefan Countryman <stefan.countryman@gmail.com>
2015-09-19 19:07:24 -0400

    Add a dashed line and todos to parallax

--- parallax/script1.js
+++ parallax/script1.js
@@ -14,6 +14,7 @@ var fullHeight = function(){
 //==============================================================================
 
 var earth_color = 'blue';
+var sun_color = 'yellow';
 var star_color  = '#cccccc';
 var line_color  = '#cccccc';
 
@@ -21,8 +22,10 @@ var line_color  = '#cccccc';
 //           SCALED LENGTHS
 //==============================================================================
 
-function radius()       {  return fullWidth() / 40;     }
-function innerRadius()  {  return fullWidth() / 160;     }
+function earthRadius()  {  return fullWidth() / 40;     }
+function sunRadius()    {  return fullWidth() / 160;    }
+function orbitRadius()  {  return fullWidth() / 40;     }
+function innerRadius()  {  return fullWidth() / 160;    }
 function outerRadius()  {  return fullWidth() / 80;     }
 function x0()           {  return fullWidth() / 10;     }
 function y0()           {  return fullWidth() / 10;     }
@@ -38,14 +41,17 @@ var ystar = y0star();       // Current y position of the star
 var anim;                   // Animation for the triangle
 var motionLayer;            // Layer for animated objects
 var triangle;               // The triangle
+var radialLine;
 var earth;
+var sun;                    // The sun
+var orbit;                  // The earth's orbit
 var star;
 
 //==============================================================================
 //           CONVENIENCE FUNCTIONS
 //==============================================================================
 
-// Get the vector pointing from earth center to star center
+// Get the vector pointing from orbit center to star center
 function r()     {  return [ xstar - x0() , ystar - y0() ]      }
 // Get it as a unit vector
 function rhat()  {
@@ -53,16 +59,16 @@ function rhat()  {
     var absr = Math.sqrt( r[0]*r[0] + r[1]*r[1] );
     return [ r[0]/absr , r[1]/absr ];
 }
-// Rotate the unit vector by +90 degrees and scale it to the earth's radius
+// Rotate the unit vector by +90 degrees and scale it to the orbit radius
 function rup() {
     var rh = rhat();
-    var rad = radius();
+    var rad = orbitRadius();
     return [ -rh[1] * rad , rh[0] * rad ];
 }
-// Rotate the unit vector by -90 degrees and scale it to the earth's radius
+// Rotate the unit vector by -90 degrees and scale it to the orbit radius
 function rdown() {
     var rh = rhat();
-    var rad = radius();
+    var rad = orbitRadius();
     return [ rh[1] * rad , -rh[0] * rad ];
 }
 // Get the vertices of the triangle
@@ -76,6 +82,11 @@ function triangleVertices() {
     downy = downr[1] + y0();
     return [ upx, upy, downx, downy, xstar, ystar ];
 }
+// TODO: Find half the angle between the two vectors
+// TODO: Animate a little earth in orbit
+// TODO: Animate the vector between the earth and the star
+// TODO: add a little diagram with the earth and the vector changing
+// TODO: add a little picture showing the star "moving" against the background
 
 //==============================================================================
 //           KONVA SETUP: BUILD THE SCENE
@@ -97,14 +108,21 @@ function buildScene() {
     var motionLayer = new Konva.Layer();
 
     /*
-    * create a circle representing earth
+    * create a circle representing earth's orbit and a smaller one for the sun
     */
 
-    earth = new Konva.Circle({
+    orbit = new Konva.Circle({
         x: x0(),
         y: y0(),
-        radius: radius(),
-        fill: 'blue'
+        radius: orbitRadius(),
+        stroke: line_color
+    });
+
+    sun = new Konva.Circle({
+        x: x0(),
+        y: y0(),
+        radius: sunRadius(),
+        fill: sun_color
     });
 
     /* 
@@ -133,16 +151,31 @@ function buildScene() {
         closed: true
     });
 
-    bgLayer.add(earth);
+    /*
+    * create the readial line connecting the center of the circle and the star
+    */
+
+    radialLine = new Konva.Line({
+        points: [x0(), y0(), xstar, ystar],
+        stroke: line_color,
+        dash: [10, 10],
+        stroke_width: 3,
+        closed: false
+    });
+
+    bgLayer.add(orbit);
+    bgLayer.add(sun);
     motionLayer.add(triangle);
+    motionLayer.add(radialLine);
     motionLayer.add(star);
 
-    stage.add(bgLayer);
     stage.add(motionLayer);
+    stage.add(bgLayer);
 
     /*
     * when the star is moved, refresh the xstar and ystar positions
     */
+    star.cache();
     star.on('dragstart', function() {
         anim.start();
     });
@@ -159,6 +192,7 @@ var anim = new Konva.Animation(function(frame) {
     xstar = star.getX();
     ystar = star.getY();
     triangle.points(triangleVertices());
+    radialLine.points([x0(), y0(), xstar, ystar]);
 }, motionLayer);
 
 //==============================================================================
